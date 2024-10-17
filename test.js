1c88062836647468c18c2ec1d99441bff460692a
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-10-17 16:25:00 +0200

    Disable verbose output

--- test.js
+++ test.js
@@ -5,13 +5,13 @@ const make = require('.')
 test('basic', { timeout: 120000 }, async (t) => {
   const cwd = path.resolve(__dirname, 'test/fixtures/basic')
 
-  const build = await make.generate({ cwd, cache: false, verbose: true, stdio: 'inherit' })
+  const build = await make.generate({ cwd, cache: false })
 
   t.is(build, path.resolve(__dirname, 'test/fixtures/basic/build'))
 
-  await make.build({ build, cwd, verbose: true, stdio: 'inherit' })
+  await make.build({ build, cwd })
 
-  const install = await make.install({ build, cwd, verbose: true, stdio: 'inherit' })
+  const install = await make.install({ build, cwd })
 
   t.is(install, path.resolve(__dirname, 'test/fixtures/basic/prebuilds'))
 })
