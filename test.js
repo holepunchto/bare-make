f50d8b8e0c801f49c2e0a2af7a46a66fdc922e55
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-10-17 15:46:24 +0200

    Log CMake output

--- test.js
+++ test.js
@@ -5,13 +5,13 @@ const make = require('.')
 test('basic', async (t) => {
   const cwd = path.resolve(__dirname, 'test/fixtures/basic')
 
-  const build = await make.generate({ cwd, cache: false })
+  const build = await make.generate({ cwd, cache: false, stdio: 'inherit' })
 
   t.is(build, path.resolve(__dirname, 'test/fixtures/basic/build'))
 
-  await make.build({ build, cwd })
+  await make.build({ build, cwd, stdio: 'inherit' })
 
-  const install = await make.install({ build, cwd })
+  const install = await make.install({ build, cwd, stdio: 'inherit' })
 
   t.is(install, path.resolve(__dirname, 'test/fixtures/basic/prebuilds'))
 })
