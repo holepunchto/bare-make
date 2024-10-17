64c3507caf9b40f4005ac5f6600567ce5bfc32f3
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-10-17 16:47:23 +0200

    Remove resolution values

--- test.js
+++ test.js
@@ -5,13 +5,9 @@ const make = require('.')
 test('basic', { timeout: 120000 }, async (t) => {
   const cwd = path.resolve(__dirname, 'test/fixtures/basic')
 
-  const build = await make.generate({ cwd, cache: false })
+  await t.execution(make.generate({ cwd, cache: false }))
 
-  t.is(build, path.resolve(__dirname, 'test/fixtures/basic/build'))
+  await t.execution(make.build({ cwd }))
 
-  await make.build({ build, cwd })
-
-  const install = await make.install({ build, cwd })
-
-  t.is(install, path.resolve(__dirname, 'test/fixtures/basic/prebuilds'))
+  await t.execution(make.install({ cwd }))
 })
