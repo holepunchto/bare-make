4e26e008f3877f55d7e4c16804900a26d88418b5
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-07 10:08:15 +0100

    Test address sanitizer builds

--- test.js
+++ test.js
@@ -7,7 +7,15 @@ test('basic', { timeout: 120000 }, async (t) => {
 
   await t.execution(make.generate({ cwd, cache: false }))
 
-  await t.execution(make.build({ cwd }))
+  await t.execution(make.build({ cwd, clean: true }))
 
   await t.execution(make.install({ cwd }))
+})
+
+test('address sanitizier', { timeout: 120000 }, async (t) => {
+  const cwd = path.resolve(__dirname, 'test/fixtures/basic')
+
+  await t.execution(make.generate({ cwd, cache: false, sanitize: 'address' }))
+
+  await t.execution(make.build({ cwd, clean: true }))
 })
