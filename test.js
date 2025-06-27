4a11611be42bf8b1e1a6c57bf2a3324f432118ea
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-06-27 14:19:35 +0200

    Skip sanitizer test on Windows

--- test.js
+++ test.js
@@ -12,13 +12,17 @@ test('basic', { timeout: 120000 }, async (t) => {
   await t.execution(make.install({ cwd }))
 })
 
-test('address sanitizier', { timeout: 120000 }, async (t) => {
-  const cwd = path.resolve(__dirname, 'test/fixtures/basic')
+test(
+  'address sanitizier',
+  { skip: Bare.platform === 'win32', timeout: 120000 },
+  async (t) => {
+    const cwd = path.resolve(__dirname, 'test/fixtures/basic')
 
-  await t.execution(make.generate({ cwd, cache: false, sanitize: 'address' }))
+    await t.execution(make.generate({ cwd, cache: false, sanitize: 'address' }))
 
-  await t.execution(make.build({ cwd, clean: true }))
-})
+    await t.execution(make.build({ cwd, clean: true }))
+  }
+)
 
 test('color', { timeout: 120000 }, async (t) => {
   const cwd = path.resolve(__dirname, 'test/fixtures/basic')
