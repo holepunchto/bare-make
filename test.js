7c40a903997ef3b6556503315fb9a2a5a336b109
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-09-22 14:21:13 +0200

    Never use shell

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 const test = require('brittle')
 const path = require('path')
-const { isWindows } = require('which-runtime')
+const { isWindows, isBare } = require('which-runtime')
 const make = require('.')
 
-test('basic', { timeout: 120000 }, async (t) => {
+test('basic', { skip: isBare, timeout: 120000 }, async (t) => {
   const cwd = path.resolve(__dirname, 'test/fixtures/basic')
 
   await t.execution(make.generate({ cwd, cache: false, stdio: 'inherit' }))
@@ -13,17 +13,26 @@ test('basic', { timeout: 120000 }, async (t) => {
   await t.execution(make.install({ cwd, stdio: 'inherit' }))
 })
 
-test('address sanitizier', { skip: isWindows, timeout: 120000 }, async (t) => {
-  const cwd = path.resolve(__dirname, 'test/fixtures/basic')
+test(
+  'address sanitizier',
+  { skip: isBare || isWindows, timeout: 120000 },
+  async (t) => {
+    const cwd = path.resolve(__dirname, 'test/fixtures/basic')
 
-  await t.execution(
-    make.generate({ cwd, cache: false, stdio: 'inherit', sanitize: 'address' })
-  )
+    await t.execution(
+      make.generate({
+        cwd,
+        cache: false,
+        stdio: 'inherit',
+        sanitize: 'address'
+      })
+    )
 
-  await t.execution(make.build({ cwd, clean: true, stdio: 'inherit' }))
-})
+    await t.execution(make.build({ cwd, clean: true, stdio: 'inherit' }))
+  }
+)
 
-test('color', { timeout: 120000 }, async (t) => {
+test('color', { skip: isBare, timeout: 120000 }, async (t) => {
   const cwd = path.resolve(__dirname, 'test/fixtures/basic')
 
   await t.execution(
